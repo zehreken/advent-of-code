ae8d7d095641eb96ab23966a7b8d7f5d32c90be8
zehreken <zehreken@gmail.com>
2022-12-09 16:57:50 +0100

    Update aoc07.rs

--- src/aoc07.rs
+++ src/aoc07.rs
@@ -19,11 +19,12 @@ fn part_one() {
     // Setting current dir to root
     let mut current_dir: String = "/".to_owned();
 
+    let mut line_index = 0;
     read_lines("input/aoc07.txt")
         .expect("Error reading aoc07.txt")
         .for_each(|line| {
             if let Ok(line) = line {
-                // println!("{}", line);
+                println!("{}: {}", line_index, line);
                 process_line(
                     line,
                     &mut current_dir,
@@ -31,19 +32,39 @@ fn part_one() {
                     &mut child_to_parent_map,
                     &mut directory_to_files_map,
                 );
+                line_index += 1;
             }
         });
 
     println!("========================");
-    for (parent, children) in parent_to_children_map {
-        let mut dir_size: u32 = 0;
-        for child in children {
-            for file in &directory_to_files_map[&child] {
-                dir_size += file.size;
-            }
+    let mut directory_to_size_map:HashMap<String, u32> = HashMap::new();
+    for (directory, _) in &directory_to_files_map {
+        let size = calculate_size(&directory, &parent_to_children_map, &directory_to_files_map);
+        if !directory_to_size_map.contains_key(directory) {
+            directory_to_size_map.insert(directory.to_owned(), size);
         }
-        println!("{}, {}", parent, dir_size);
+        println!("directory {} is {}", directory, size);
     }
+
+}
+
+fn calculate_size(
+    directory: &String,
+    parent_to_children_map: &HashMap<String, Vec<String>>,
+    directory_to_files_map: &HashMap<String, Vec<File>>,
+) -> u32 {
+    let mut dir_size = 0;
+    for file in &directory_to_files_map[directory] {
+        dir_size += file.size;
+    }
+    if parent_to_children_map.contains_key(directory) {
+        let children = parent_to_children_map.get(directory).unwrap();
+        for child in children {
+            dir_size += calculate_size(child, parent_to_children_map, directory_to_files_map)
+        }
+    }
+    // println!("directory {} is {}", directory, dir_size);
+    dir_size
 }
 
 fn process_line(
@@ -63,20 +84,20 @@ fn process_line(
                     let third = split.next();
                     if let Some(third) = third {
                         if third == ".." {
-                            println!("Go one level up");
+                            println!("Go one level up {} current {}", child_to_parent_map[current_dir], current_dir);
                             *current_dir = child_to_parent_map[current_dir].to_owned();
                         } else {
-                            println!("Going in {}", third);
+                            println!("Going in {}, current {}", third, current_dir);
                             *current_dir = third.to_owned();
                         }
                     }
                 } else if second == "ls" {
-                    println!("Skipping ls, this is only for humans anyways");
+                    // println!("Skipping ls, this is only for humans anyways");
                 }
             }
         } else if first == "dir" {
-            let second = split.next();
-            if let Some(second) = second {
+            let dir_name = split.next();
+            if let Some(dir_name) = dir_name {
                 println!("Create dir");
                 if !parent_to_children_map.contains_key(current_dir) {
                     parent_to_children_map.insert(current_dir.to_owned(), vec![]);
@@ -84,9 +105,9 @@ fn process_line(
                 parent_to_children_map
                     .get_mut(current_dir)
                     .unwrap()
-                    .push(second.to_owned());
-                if !child_to_parent_map.contains_key(second) {
-                    child_to_parent_map.insert(second.to_owned(), current_dir.to_owned());
+                    .push(dir_name.to_owned());
+                if !child_to_parent_map.contains_key(dir_name) {
+                    child_to_parent_map.insert(dir_name.to_owned(), current_dir.to_owned());
                 }
             }
         } else {
