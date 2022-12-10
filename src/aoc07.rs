e4cc2a235cc7eac12882d33c87e41a46b47f38d0
zehreken <zehreken@gmail.com>
2022-12-10 21:02:49 +0100

    Update aoc07.rs

--- src/aoc07.rs
+++ src/aoc07.rs
@@ -1,134 +1,138 @@
-use std::collections::HashMap;
-
 use crate::utils::read_lines;
+use std::{cell::RefCell, rc::Rc, vec};
 
 pub fn run() {
     part_two();
     part_one();
 }
 
-fn part_two() {}
-
-fn part_one() {
-    let mut parent_to_children_map: HashMap<String, Vec<String>> = HashMap::new();
-    let mut child_to_parent_map: HashMap<String, String> = HashMap::new();
-    let mut directory_to_files_map: HashMap<String, Vec<File>> = HashMap::new();
-
-    // Adding root dir
-    parent_to_children_map.insert("/".to_owned(), vec![]);
-    // Setting current dir to root
-    let mut current_dir: String = "/".to_owned();
-
-    let mut line_index = 0;
-    read_lines("input/aoc07.txt")
-        .expect("Error reading aoc07.txt")
-        .for_each(|line| {
-            if let Ok(line) = line {
-                println!("{}: {}", line_index, line);
-                process_line(
-                    line,
-                    &mut current_dir,
-                    &mut parent_to_children_map,
-                    &mut child_to_parent_map,
-                    &mut directory_to_files_map,
-                );
-                line_index += 1;
-            }
-        });
-
-    println!("========================");
-    let mut directory_to_size_map:HashMap<String, u32> = HashMap::new();
-    for (directory, _) in &directory_to_files_map {
-        let size = calculate_size(&directory, &parent_to_children_map, &directory_to_files_map);
-        if !directory_to_size_map.contains_key(directory) {
-            directory_to_size_map.insert(directory.to_owned(), size);
-        }
-        println!("directory {} is {}", directory, size);
-    }
-
+fn part_two() {
+    let root = part_one();
+    const DISK_SPACE: u32 = 70_000_000;
+    const EMPTY_SPACE_NEEDED: u32 = 30_000_000;
+    let current_used_space = calculate_size(&root, &mut 0);
+    let necessary_space = current_used_space - (DISK_SPACE - EMPTY_SPACE_NEEDED);
+    println!("{}", necessary_space);
+    let mut current_max = 0;
+    calculate_size_for_necessary_space(&root, necessary_space, &mut current_max);
 }
 
-fn calculate_size(
-    directory: &String,
-    parent_to_children_map: &HashMap<String, Vec<String>>,
-    directory_to_files_map: &HashMap<String, Vec<File>>,
+fn calculate_size_for_necessary_space(
+    dir: &Rc<RefCell<Directory>>,
+    necessary_space: u32,
+    current_max: &mut u32,
 ) -> u32 {
-    let mut dir_size = 0;
-    for file in &directory_to_files_map[directory] {
-        dir_size += file.size;
+    let mut dir_size = dir.borrow().get_size();
+    let children = &dir.borrow().children;
+    for d in children {
+        dir_size += calculate_size_for_necessary_space(d, necessary_space, current_max);
     }
-    if parent_to_children_map.contains_key(directory) {
-        let children = parent_to_children_map.get(directory).unwrap();
-        for child in children {
-            dir_size += calculate_size(child, parent_to_children_map, directory_to_files_map)
-        }
+
+    if dir_size <= necessary_space + 30000 && dir_size > *current_max {
+        *current_max = dir_size;
+        println!("dir size: {}", dir_size);
     }
-    // println!("directory {} is {}", directory, dir_size);
+
     dir_size
 }
 
-fn process_line(
-    line: String,
-    current_dir: &mut String,
-    parent_to_children_map: &mut HashMap<String, Vec<String>>,
-    child_to_parent_map: &mut HashMap<String, String>,
-    directory_to_files_map: &mut HashMap<String, Vec<File>>,
-) {
-    let mut split = line.split(' ');
-    let first = split.next();
-    if let Some(first) = first {
-        if first == "$" {
-            let second = split.next();
-            if let Some(second) = second {
-                if second == "cd" {
-                    let third = split.next();
-                    if let Some(third) = third {
-                        if third == ".." {
-                            println!("Go one level up {} current {}", child_to_parent_map[current_dir], current_dir);
-                            *current_dir = child_to_parent_map[current_dir].to_owned();
-                        } else {
-                            println!("Going in {}, current {}", third, current_dir);
-                            *current_dir = third.to_owned();
+fn part_one() -> Rc<RefCell<Directory>> {
+    let root = Rc::new(RefCell::new(Directory {
+        name: "/".to_owned(),
+        parent: None,
+        children: vec![],
+        files: vec![],
+    }));
+    let mut current_dir = Rc::clone(&root);
+    read_lines("input/aoc07.txt")
+        .expect("Error reading aoc07.txt")
+        .skip(1)
+        .for_each(|line| {
+            let line = line.unwrap();
+            let mut split = line.split(' ');
+            let first = split.next();
+            if let Some(first) = first {
+                if first == "$" {
+                    let command = split.next();
+                    if let Some(command) = command {
+                        if command == "cd" {
+                            let path = split.next();
+                            if let Some(path) = path {
+                                if path == ".." {
+                                    let current_clone = Rc::clone(&current_dir);
+                                    current_dir =
+                                        Rc::clone(current_clone.borrow().parent.as_ref().unwrap());
+                                } else {
+                                    let new_dir = Rc::new(RefCell::new(Directory {
+                                        name: path.to_owned(),
+                                        parent: Some(Rc::clone(&current_dir)),
+                                        children: vec![],
+                                        files: vec![],
+                                    }));
+                                    current_dir.borrow_mut().children.push(Rc::clone(&new_dir));
+                                    current_dir = Rc::clone(&new_dir);
+                                }
+                            }
+                        } else if command == "ls" {
                         }
                     }
-                } else if second == "ls" {
-                    // println!("Skipping ls, this is only for humans anyways");
+                } else if first == "dir" {
+                    let dir_name = split.next();
+                    if let Some(dir_name) = dir_name {
+                        // println!("Create dir");
+                    }
+                } else {
+                    let file_name = split.next();
+                    if let Some(file_name) = file_name {
+                        let file = File {
+                            name: file_name.to_owned(),
+                            size: first.parse::<u32>().unwrap(),
+                        };
+                        current_dir.borrow_mut().files.push(file);
+                    }
                 }
             }
-        } else if first == "dir" {
-            let dir_name = split.next();
-            if let Some(dir_name) = dir_name {
-                println!("Create dir");
-                if !parent_to_children_map.contains_key(current_dir) {
-                    parent_to_children_map.insert(current_dir.to_owned(), vec![]);
-                }
-                parent_to_children_map
-                    .get_mut(current_dir)
-                    .unwrap()
-                    .push(dir_name.to_owned());
-                if !child_to_parent_map.contains_key(dir_name) {
-                    child_to_parent_map.insert(dir_name.to_owned(), current_dir.to_owned());
-                }
-            }
-        } else {
-            let second = split.next();
-            if let Some(second) = second {
-                if !directory_to_files_map.contains_key(current_dir) {
-                    directory_to_files_map.insert(current_dir.to_owned(), vec![]);
-                }
-                directory_to_files_map
-                    .get_mut(current_dir)
-                    .unwrap()
-                    .push(File {
-                        size: first.parse::<u32>().unwrap(),
-                        name: second.to_owned(),
-                    })
-            }
-        }
+        });
+
+    let mut sum_of_dirs_have_size_under_100k = 0;
+    calculate_size(&root, &mut sum_of_dirs_have_size_under_100k);
+    // let children: &Vec<Rc<RefCell<Directory>>> = &root.borrow().children;
+    // for d in children.iter() {
+    //     calculate_size(d);
+    // }
+    // println!("{}", sum_of_dirs_have_size_under_100k);
+    root
+}
+
+fn calculate_size(dir: &Rc<RefCell<Directory>>, sum_of_dirs_have_size_under_100k: &mut u32) -> u32 {
+    let mut dir_size = dir.borrow().get_size();
+    let children = &dir.borrow().children;
+    for d in children {
+        dir_size += calculate_size(d, sum_of_dirs_have_size_under_100k);
+    }
+
+    let name = &dir.borrow().name;
+    // println!("{} {}", name, dir_size);
+    if dir_size < 100_000 {
+        *sum_of_dirs_have_size_under_100k += dir_size;
+    }
+
+    dir_size
+}
+
+struct Directory {
+    name: String,
+    parent: Option<Rc<RefCell<Directory>>>,
+    children: Vec<Rc<RefCell<Directory>>>,
+    files: Vec<File>,
+}
+
+impl Directory {
+    fn get_size(&self) -> u32 {
+        self.files.iter().map(|f| f.size).sum()
     }
 }
 
-#[derive(Debug)]
 struct File {
     name: String,
     size: u32,
