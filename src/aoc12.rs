392a3e0cc84b50602372b5ddb8d8901db22a39e3
zehreken <zehreken@gmail.com>
2022-12-13 00:39:11 +0100

    Update aoc12.rs

--- src/aoc12.rs
+++ src/aoc12.rs
@@ -11,20 +11,64 @@ pub fn run() {
 fn part_two() {}
 
 fn part_one() {
-    let mut grid: Vec<Vec<u8>> = vec![];
+    let mut grid: Vec<Vec<Cell>> = vec![];
+    let mut row_count = 0;
+    let mut column_count = 0;
     read_lines("input/aoc12.txt")
         .expect("Error reading aoc12.txt")
         .for_each(|l| {
-            let mut row: Vec<u8> = vec![];
+            let mut row: Vec<Cell> = vec![];
             l.unwrap().chars().for_each(|c| {
                 let height = c as u8 - 48;
-                row.push(height);
+                let cell = Cell {
+                    r: row_count,
+                    c: column_count,
+                    height,
+                    h_score: calculate_h_score((row_count, column_count), (3, 6)),
+                    g_score: 0,
+                };
+                row.push(cell);
+                column_count += 1;
             });
             grid.push(row);
+            column_count = 0;
+            row_count += 1;
         });
 
     grid.iter().for_each(|row| {
-        row.iter().for_each(|c| print!("{} ", c));
+        row.iter().for_each(|cell| print!("{:?} ", cell.height));
         println!("");
     });
+
+    println!("");
+
+    grid.iter().for_each(|row| {
+        row.iter().for_each(|cell| print!("{:?} ", cell.h_score));
+        println!("");
+    });
+
+    let mut visited_cells: Vec<&Cell> = vec![];
+    let mut frontier_cells: Vec<&mut Cell> = vec![];
+    frontier_cells.push(&mut grid[0][0]);
+}
+
+fn check_frontier_cells(frontier_cells: &mut Vec<&mut Cell>, visited_cells: &mut Vec<&Cell>) {}
+
+fn calculate_h_score((r1, c1): (u8, u8), (r2, c2): (u8, u8)) -> u8 {
+    r1.abs_diff(r2) + c1.abs_diff(c2)
+}
+
+#[derive(Debug)]
+struct Cell {
+    r: u8,
+    c: u8,
+    height: u8,
+    h_score: u8,
+    g_score: u8,
+}
+
+impl Cell {
+    fn get_f_score(&self) -> u8 {
+        self.h_score + self.g_score
+    }
 }
