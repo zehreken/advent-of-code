6bb2706bdfa4d8f5bc8952cb2966cbafca203ca6
zehreken <zehreken@gmail.com>
2022-12-14 23:26:58 +0100

    Update aoc12.rs

--- src/aoc12.rs
+++ src/aoc12.rs
@@ -50,6 +50,37 @@ fn part_one() {
     let mut visited_cells: Vec<&Cell> = vec![];
     let mut frontier_cells: Vec<&mut Cell> = vec![];
     frontier_cells.push(&mut grid[0][0]);
+
+    grid.iter().for_each(|row| {
+        row.iter().for_each(|cell| {
+            println!("{:?}", get_neighbours(&grid, &cell));
+            print!("{:?} ", cell.height);
+        });
+    });
+}
+
+fn get_neighbours<'a>(grid: &'a Vec<Vec<Cell>>, cell: &'a Cell) -> Vec<&'a Cell> {
+    // up, right, down, left
+    let neighbour_coords: Vec<(i8, i8)> = vec![(0, -1), (1, 0), (0, 1), (-1, 0)];
+    let mut neighbours = vec![];
+    let row = cell.r;
+    let column = cell.c;
+
+    for i in 0..grid.len() {
+        for j in 0..grid[i].len() {
+            for (n_r, n_c) in &neighbour_coords {
+                if grid[i][j].r as i8 + n_r >= 0
+                    && grid[i][j].r as i8 + n_r < grid.len() as i8
+                    && grid[i][j].c as i8 + n_c >= 0
+                    && grid[i][j].c as i8 + n_c < grid[i].len() as i8
+                {
+                    neighbours.push(&grid[i][j]);
+                }
+            }
+        }
+    }
+
+    neighbours
 }
 
 fn check_frontier_cells(frontier_cells: &mut Vec<&mut Cell>, visited_cells: &mut Vec<&Cell>) {}
