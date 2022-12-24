16c36d2e847df9356c517cfbf926ec7f8850cfed
zehreken <zehreken@gmail.com>
2022-12-24 01:29:30 +0100

    Add part one of aoc12.rs

--- src/aoc12.rs
+++ src/aoc12.rs
@@ -2,8 +2,8 @@ use crate::utils::read_lines;
 use std::collections::{HashMap, VecDeque};
 use std::rc::Rc;
 
-const START: i16 = 48;
-const END: i16 = 75;
+const START: char = 'S';
+const END: char = 'E';
 
 pub fn run() {
     part_two();
@@ -13,7 +13,7 @@ pub fn run() {
 fn part_two() {}
 
 fn part_one() {
-    let mut grid: Vec<Vec<i16>> = vec![];
+    let mut grid: Vec<Vec<char>> = vec![];
     let mut row_count: i32 = 0;
     let mut column_count: i32 = 0;
     let mut start_pos = Vec2 { row: 0, column: 0 };
@@ -21,23 +21,16 @@ fn part_one() {
     read_lines("input/aoc12.txt")
         .expect("Error reading aoc12.txt")
         .for_each(|l| {
-            let mut row: Vec<i16> = vec![];
+            let mut row: Vec<char> = vec![];
             column_count = 0;
             l.unwrap().chars().for_each(|c| {
-                if c == 'S' {
+                if c == START {
                     start_pos = Vec2 {
                         row: row_count,
                         column: column_count,
                     };
                 }
-                let height = if c == 'S' {
-                    48
-                } else if c == 'E' {
-                    75
-                } else {
-                    c as i16 - 48
-                };
-                row.push(height);
+                row.push(c);
                 column_count += 1;
             });
             grid.push(row);
@@ -51,7 +44,6 @@ fn part_one() {
             row: start_pos.row,
             column: start_pos.column,
         },
-        height: START,
         parent: None,
     };
     visited.push(root.pos);
@@ -65,7 +57,6 @@ fn part_one() {
         if let Some(current_pos) = current_pos {
             let current_height = grid[current_pos.row as usize][current_pos.column as usize];
             if current_height == END {
-                //
                 println!("Reached the target!");
                 // print_state(&grid, current_pos, &visited);
                 target = Some(Rc::clone(&pos_to_node[&current_pos]));
@@ -76,13 +67,12 @@ fn part_one() {
                 if !visited.iter().any(|n| *n == neighbour) {
                     let current_node = Rc::clone(&pos_to_node[&current_pos]);
                     let n_height = grid[neighbour.row as usize][neighbour.column as usize];
-                    if n_height - current_height <= 1 {
+                    if current_height == START || n_height as i8 - current_height as i8 <= 1 {
                         let child = Node {
                             pos: Vec2 {
                                 row: neighbour.row,
                                 column: neighbour.column,
                             },
-                            height: n_height,
                             parent: Some(current_node),
                         };
                         visited.push(neighbour);
@@ -97,11 +87,11 @@ fn part_one() {
 
     let mut path = vec![];
     let mut node_count = 0;
-    let mut cache = &target;
+    let mut next = &target;
     'a: loop {
-        if let Some(current) = cache {
+        if let Some(current) = next {
             path.push(current.pos);
-            cache = &current.parent;
+            next = &current.parent;
             node_count += 1;
         } else {
             break 'a;
@@ -110,24 +100,18 @@ fn part_one() {
     print_path(&grid, &path);
     // Step count is 1 less than node count
     println!("{}", node_count);
-
-    // if let Some(target) = target {
-    //     println!("{:?}", target);
-    // } else {
-    //     println!("Couldn't reach target!");
-    // }
 }
 
-fn print_path(grid: &Vec<Vec<i16>>, path: &Vec<Vec2>) {
+fn print_path(grid: &Vec<Vec<char>>, path: &Vec<Vec2>) {
     grid.iter().enumerate().for_each(|(r, row)| {
         row.iter().enumerate().for_each(|(c, height)| {
             if path
                 .iter()
                 .any(|n| n.row == r as i32 && n.column == c as i32)
             {
-                print!("** ");
+                print!("\x1b[93m{}\x1b[0m", height);
             } else {
-                print!("{:?} ", height);
+                print!("{}", height);
             }
         });
         println!("");
@@ -135,18 +119,18 @@ fn print_path(grid: &Vec<Vec<i16>>, path: &Vec<Vec2>) {
     println!("");
 }
 
-fn print_state(grid: &Vec<Vec<i16>>, current_pos: Vec2, visited: &Vec<Vec2>) {
+fn print_state(grid: &Vec<Vec<char>>, current_pos: Vec2, visited: &Vec<Vec2>) {
     grid.iter().enumerate().for_each(|(r, row)| {
         row.iter().enumerate().for_each(|(c, height)| {
             if current_pos.row == r as i32 && current_pos.column == c as i32 {
-                print!("{:?}_", height);
+                print!("_");
             } else if visited
                 .iter()
                 .any(|n| n.row == r as i32 && n.column == c as i32)
             {
-                print!("** ");
+                print!("*");
             } else {
-                print!("{:?} ", height);
+                print!("{}", height);
             }
         });
         println!("");
@@ -155,8 +139,9 @@ fn print_state(grid: &Vec<Vec<i16>>, current_pos: Vec2, visited: &Vec<Vec2>) {
 }
 
 fn get_neighbours(pos: Vec2, row_count: i32, column_count: i32) -> Vec<Vec2> {
-    // up, right, down, left
-    let neighbour_coords: Vec<(i32, i32)> = vec![(-1, 0), (0, 1), (1, 0), (0, -1)];
+    // left, down, right, up
+    let neighbour_coords: Vec<(i32, i32)> = vec![(0, -1), (1, 0), (0, 1), (-1, 0)];
+
     let mut neighbours = vec![];
 
     for (n_r, n_c) in neighbour_coords {
@@ -175,30 +160,9 @@ fn get_neighbours(pos: Vec2, row_count: i32, column_count: i32) -> Vec<Vec2> {
     neighbours
 }
 
-fn _get_neighbours<'a>(grid: &'a Vec<Vec<Node>>, cell: &Node) -> Vec<Vec2> {
-    // up, right, down, left
-    let neighbour_coords: Vec<(i8, i8)> = vec![(-1, 0), (0, 1), (1, 0), (0, -1)];
-    let mut neighbours = vec![];
-    let row = cell.pos.row;
-    let column = cell.pos.column;
-
-    for r in 0..grid.len() {
-        for c in 0..grid[r].len() {
-            for (n_r, n_c) in &neighbour_coords {
-                if row as i8 + n_r == r as i8 && column as i8 + n_c == c as i8 {
-                    neighbours.push(grid[r][c].pos);
-                }
-            }
-        }
-    }
-
-    neighbours
-}
-
 #[derive(Debug)]
 struct Node {
     pos: Vec2,
-    height: i16,
     parent: Option<Rc<Node>>,
 }
 
